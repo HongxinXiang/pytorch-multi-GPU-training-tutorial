39a128e2ee838141289bdafb04607b6ee1aba54b
hongxin xiang <xianghx21@gmail.com>
2022-07-17 12:58:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Multi-GPU training tutorial based on PyTorch
+
+## Environment Installation
+
+#### 1. GPU environment
+CUDA 10.1
+
+#### 2. create a new conda environment
+conda create -n pytorch-multi-GPU-training-tutorial python=3.7.3\
+conda activate pytorch-multi-GPU-training-tutorial
+
+#### 3. download some packages 
+
---
+++ single-machine-and-multi-GPU-DataParallel.py
@@ -0,0 +1,73 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from model import NeuralNetwork
+
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)  # copy tensor from cpu to gpu
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)  # copy tensor from cpu to gpu
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+if __name__ == '__main__':
+    # initialize dataset
+    training_data = datasets.FashionMNIST(root="data", train=True, download=True, transform=ToTensor())
+    test_data = datasets.FashionMNIST(root="data", train=False, download=True, transform=ToTensor())
+
+    # initialize data loader
+    batch_size = 64
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    # Get cpu or gpu device for training.
+    device = "cuda" if torch.cuda.is_available() else "cpu"  # using GPU
+    print(f"Using {device} device")
+
+    # initialize model
+    model = NeuralNetwork().to(device)
+    print(model)
+
+    # initialize optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    # train on single-GPU
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test(test_dataloader, model, loss_fn)
+    print("Done!")
